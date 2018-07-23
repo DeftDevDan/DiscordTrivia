3f8599549d0aff6aad55eda608db8b8fce274cea
Daniel Kang <mrdanielkang@gmail.com>
2018-07-22 17:57:37 -0700

    Add Testing

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ const PORT = process.env.PORT;
 
 bot.registry.registerGroup('trivia', 'Trivia');
 bot.registry.registerGroup('user', 'User');
-bot.registry.registerGroup('test', 'Test');
+
+if (process.env.TESTING) {
+  bot.registry.registerGroup('test', 'Test');
+}
+
 bot.registry.registerGroup('channel', 'Channel');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
@@ -24,25 +28,11 @@ bot.login(process.env.TOKEN);
 
 console.log('Server has started and bot has logged on!');
 
-// bot.joinVoiceChannel('General');
-
 bot.on('voiceStateUpdate', (oldMember, newMember) => {
     let newUserChannel = newMember.voiceChannel
     let oldUserChannel = oldMember.voiceChannel
   
-  
     if(oldUserChannel === undefined && newUserChannel !== undefined) {
-  
-       // User Joins a voice channel
-
-      
-      // console.log(newUserChannel.name);
-      
-      // console.log(oldMember.user.username);
-      // if(oldMember.user.username === 'KptKid') {
-        
-      //   // bot.voiceConnection.playFile('./sounds/'+oldMember.user.username+'.mp3');
-      // }
 
       newUserChannel.join().then(connection => {
         const dispatcher = connection.playFile('./sounds/'+oldMember.user.username+'.mp3');
